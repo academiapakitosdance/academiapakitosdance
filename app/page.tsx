1966e0204c9dfcf65d6e5a0e47c441af8d232966
Training-Site <131490722+Nunes025@users.noreply.github.com>
2025-12-14 17:10:36 -0300

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
 import SatisfactionSurvey from "./satisfaction-survey"
+
+export const metadata = {
+  title: "Academia Pakitos Dance - Pesquisa de Satisfação",
+  description:
+    "Sua opinião é muito importante para nós! Ajude-nos a melhorar a Academia Pakitos Dance com sua avaliação.",
+}
+
 export default function Home() {
-return <SatisfactionSurvey />
+  return <SatisfactionSurvey />
 }
