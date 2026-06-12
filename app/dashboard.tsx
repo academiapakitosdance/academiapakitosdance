1ee0c9a393c9492c7eabbd07f1ddfa47d7bcc4a1
Training-Site <131490722+Nunes025@users.noreply.github.com>
2026-06-12 15:59:48 -0300

    Update dashboard.tsx

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -19,16 +19,16 @@ export default function Dashboard({ onBack }: DashboardProps) {
 
   // Atendimento
   const serviceData = [
-    { option: "Excelente, até indicaria para uma pessoa", votes: 43, color: "bg-green-500" },
-    { option: "Ótimo, excelente trabalho", votes: 35, color: "bg-blue-500" },
-    { option: "Bom, mas daria para melhorar", votes: 5, color: "bg-yellow-500" },
+    { option: "Excelente, até indicaria para uma pessoa", votes: 51, color: "bg-green-500" },
+    { option: "Ótimo, excelente trabalho", votes: 37, color: "bg-blue-500" },
+    { option: "Bom, mas daria para melhorar", votes: 7, color: "bg-yellow-500" },
     { option: "Ruim, precisa de melhora", votes: 0, color: "bg-red-500" },
   ]
 
   // Infraestrutura
   const infrastructureData = [
-    { option: "Muito bonita, não precisa mudar nada!", votes: 67, color: "bg-green-500" },
-    { option: "Boa, mas poderia ter alguns ajustes", votes: 14, color: "bg-blue-500" },
+    { option: "Muito bonita, não precisa mudar nada!", votes: 74, color: "bg-green-500" },
+    { option: "Boa, mas poderia ter alguns ajustes", votes: 19, color: "bg-blue-500" },
     { option: "Regular, precisa de melhorias em alguns pontos", votes: 2, color: "bg-yellow-500" },
     { option: "Ruim, precisa de uma reforma urgente", votes: 0, color: "bg-red-500" },
   ]
